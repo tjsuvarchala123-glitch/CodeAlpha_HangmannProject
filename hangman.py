855933c0e618b0c66ddc4dea296b88458be6f180
tjsuvarchala123-glitch <tjsuvarchala123@gmail.com>
2026-05-11 12:02:36 +0530

    hangman.py

--- hangman.py
+++ hangman.py
@@ -1,20 +1,15 @@
 import random
 
-# List of words
 words = ["python", "apple", "house", "tiger", "music"]
 
-# Random word selection
 word = random.choice(words)
 
-# Variables
 guessed_letters = []
 incorrect_guesses = 0
 max_incorrect = 6
 
-# Hidden word display
 display_word = ["_"] * len(word)
 
-# Hangman stages
 hangman_stages = [
     """
      -----
@@ -89,29 +84,22 @@ hangman_stages = [
 
 print("===== HANGMAN GAME =====")
 
-# Game loop
 while incorrect_guesses < max_incorrect and "_" in display_word:
 
-    # Show hangman drawing
     print(hangman_stages[incorrect_guesses])
 
-    # Show current word
     print("Word:", " ".join(display_word))
 
-    # Show remaining guesses
     print("Incorrect guesses left:", max_incorrect - incorrect_guesses)
 
-    # User input
     guess = input("Enter a letter: ").lower()
 
-    # Check repeated guess
     if guess in guessed_letters:
         print("You already guessed that letter!")
         continue
 
     guessed_letters.append(guess)
-
-    # Correct guess
+    
     if guess in word:
         print("Correct guess!")
 
@@ -119,12 +107,10 @@ while incorrect_guesses < max_incorrect and "_" in display_word:
             if word[i] == guess:
                 display_word[i] = guess
 
-    # Wrong guess
     else:
         print("Wrong guess!")
         incorrect_guesses += 1
 
-# Final result
 if "_" not in display_word:
     print("\nCongratulations! You guessed the word:", word)
 else:
